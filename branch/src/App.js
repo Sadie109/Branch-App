d1b3e2036db9406a50bad27f309875a74770b2d4
Sadie109 <101196558+Sadie109@users.noreply.github.com>
2023-03-08 18:06:30 +0000

    working on setting up client and channel

--- branch/src/App.js
+++ branch/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
+import { Chat, Channel, ChannelList } from "stream-chat-react";
+import "@stream-io/stream-chat-css/dist/css/index.css";
+import Auth from "./components/Auth.js";
+import MessagingContainer from "./components/MessagingContainer.js";
+import Video from "./components/Video.js"
 
-function App() {
+const filters = { type: "messaging" };
+const options = { state: true, presence: true, limit: 10 };
+const sort = { last_message_at: -1 };
+
+const App = () => {
+  const [client, setClient] = useState(false);
+  const [channel, setChannel] = useState(null);
+
+  const authToken = false;
+
+  useEffect(() => {
+    const setupClient = async () => {
+      const newClient = new StreamChat("bhz23f4j7esm");
+
+      const handleConnectionChange = ({ online = false }) => {
+        if (!online) return console.log("connection lost");
+        setClient(newClient);
+      };
+
+      newClient.on("connection.changed", handleConnectionChange);
+
+      await newClient.connectUser(
+        {
+          id: "dave-matthews",
+          name: "Dave Matthews",
+        },
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGF2ZS1tYXR0aGV3cyJ9.S1-S5zmpaDRZ18ON5WnA6777hrMyh9JuiA7bkH1uCoQ"
+      );
+
+      const channel = client.channel("gaming", "Branch", {
+        name: "Branch",
+      });
+
+      await channel.create();
+
+      return () => {
+        newClient.off("connection.changed", handleConnectionChange);
+        newClient.disconnectUser().then(() => console.log("connection closed"));
+      };
+    };
+    setupClient();
+    setChannel();
+  }, []);
+
+  if (!client) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!authToken && <Auth/>}
+      {authToken && (
+        <Chat client={client} darkMode={true}>
+          <ChannelList filters={filters} sort={sort} options={options} />
+          <Channel channel={channel}>
+          <Video/>
+            <MessagingContainer />
+          </Channel>
+        </Chat>
+      )}
+    </>
   );
-}
+};
 
 export default App;
